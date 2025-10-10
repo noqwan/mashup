02a2de6ca97ed7bc8e0611ea53cb045b1d161741
ymanfaloti <ymanfaloti@port-hp-elitebook650G9-13>
2025-10-10 17:42:44 +0200

    feat: add converter for internal and virtual (wrong id system for now)

--- virtualcrm/src/main/java/org/virtual/dto/converter/InternalDtoConverter.java
+++ virtualcrm/src/main/java/org/virtual/dto/converter/InternalDtoConverter.java
@@ -2,16 +2,49 @@ package org.virtual.dto.converter;
 
 import org.virtual.dto.InternalLeadDTO;
 import org.virtual.dto.VirtualLeadDTO;
+import org.virtual.dto.PositionDTO;
 
 public class InternalDtoConverter implements DtoConverter<InternalLeadDTO> {
 
   @Override
   public VirtualLeadDTO convertToVirtual(InternalLeadDTO dto) {
-    return null;
+    String[] names = dto.getLastNameFirstName().split(", ", 2);
+    String lastName = names.length > 0 ? names[0] : "";
+    String firstName = names.length > 1 ? names[1] : "";
+
+    return new VirtualLeadDTO(
+        dto.getId(),
+        firstName,
+        lastName,
+        dto.getAnnualRevenue(),
+        dto.getPhone(),
+        dto.getCreationDate(),
+        dto.getCompany(),
+        dto.getState(),
+        dto.getStreet(),
+        dto.getPostalCode(),
+        dto.getCity(),
+        dto.getCountry(),
+        null
+    );
   }
 
   @Override
   public InternalLeadDTO convertFromVirtual(VirtualLeadDTO dto) {
-    return null;
+    String lastNameFirstName = dto.getLastName() + ", " + dto.getFirstName();
+
+    return new InternalLeadDTO(
+        dto.getId(),
+        lastNameFirstName,
+        dto.getAnnualRevenue(),
+        dto.getPhone(),
+        dto.getCreationDate(),
+        dto.getCompany(),
+        dto.getState(),
+        dto.getStreet(),
+        dto.getPostalCode(),
+        dto.getCity(),
+        dto.getCountry()
+    );
   }
 }
