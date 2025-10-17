a33fc668cd39c026f57f056483f1465c0fcc2c05
tlehenaff <lehenafftangi@gmail.com>
2025-10-17 12:00:23 +0200

    "ajout méthode pour obtenir le token d'authentification Salesforce a partir d'un fichier properties"

--- virtualcrm/src/main/java/org/virtual/service/crm/SalesForceClient.java
+++ virtualcrm/src/main/java/org/virtual/service/crm/SalesForceClient.java
@@ -1,5 +1,16 @@
 package org.virtual.service.crm;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Properties;
 import org.virtual.dto.SaleForceLeadDTO;
 import org.virtual.dto.VirtualLeadDTO;
 import org.virtual.dto.converter.SaleForceDtoConverter;
@@ -7,13 +18,34 @@ import java.util.List;
 
 public class SalesForceClient extends CRMClient<SaleForceLeadDTO> {
 
+  private String authorizationToken;
+  private String uri;
+  private Properties salesForceProperties;
   public SalesForceClient() {
     this.converter = new SaleForceDtoConverter();
+    authorizationToken="";
+
+
+    String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    String salesForcePropertiesPath = rootPath + "conf/salesforce.properties";
+  try {
+    salesForceProperties = new Properties();
+    salesForceProperties.load(new FileInputStream(salesForcePropertiesPath));
+
+    uri="https://"+salesForceProperties.getProperty("salesforce_uri");
+  }
+  catch(IOException e){
+    e.printStackTrace();
+  }
   }
 
   @Override
   protected List<SaleForceLeadDTO> findLeadsSpecific() {
-    return List.of();
+    List<SaleForceLeadDTO> result = new ArrayList<>();
+
+
+
+    return result;
   }
 
   @Override
@@ -39,5 +71,36 @@ public class SalesForceClient extends CRMClient<SaleForceLeadDTO> {
   @Override
   protected SaleForceLeadDTO convertFromVirtual(VirtualLeadDTO virtualLead) {
     return this.converter.convertFromVirtual(virtualLead);
+  }
+
+  private void postForAuthorizationToken(){
+    var values = new HashMap<String, String>() {{
+      put("grant_type", "password");
+      put("client_id", salesForceProperties.getProperty("client_id"));
+      put("client_secret",salesForceProperties.getProperty("client_secret"));
+      put("username",salesForceProperties.getProperty("username"));
+      put("password", salesForceProperties.getProperty("password"));
+    }};
+
+    var objectMapper = new ObjectMapper();
+
+
+    try{
+      HttpClient client = HttpClient.newHttpClient();
+      String requestBody = objectMapper.writeValueAsString(values);
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create(uri))
+          .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+          .build();
+
+      HttpResponse<String> response = client.send(request,
+          HttpResponse.BodyHandlers.ofString());
+
+      System.out.println(response.body());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
